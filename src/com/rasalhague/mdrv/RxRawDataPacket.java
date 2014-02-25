f84b39601aa432cb6b27f26f9d670d7bcbd18426
RasAlhague <alexgr.dev@gmail.com>
2014-02-25 13:51:55 +0200

    Added: javadoc packetLength pointsAmount

--- src/com/rasalhague/mdrv/RxRawDataPacket.java
+++ src/com/rasalhague/mdrv/RxRawDataPacket.java
@@ -2,12 +2,18 @@ package com.rasalhague.mdrv;
 
 import java.util.Date;
 
+/**
+ * Class used for gson serializing
+ * Contain data about received data that encapsulate in packet
+ */
 public class RxRawDataPacket
 {
     private String rawDataPacket;
     private DeviceInfo deviceInfo;
     private Date packetCreationDate;
     private long packetCreationTimeMs;
+    private int packetLength;
+    private int pointsAmount;
 
     //Getter
     public String getRawDataPacketValue()
@@ -16,13 +22,30 @@ public class RxRawDataPacket
     }
 
     //Constructor
-    public RxRawDataPacket(String rawDataPacket, DeviceInfo deviceInfo)
+    public RxRawDataPacket(String rawData, DeviceInfo deviceInfo)
     {
-        this.rawDataPacket = rawDataPacket;
+        this.rawDataPacket = rawData;
         this.deviceInfo = deviceInfo;
 
         this.packetCreationDate = new Date();
         this.packetCreationTimeMs = this.packetCreationDate.getTime();
+        this.packetLength = rawData.length();
+
+        //TODO device switching
+        if (deviceInfo.deviceType == DeviceInfo.DeviceTypeEnum.COM)
+        {
+            char[] chars = rawData.toCharArray();
+            int cCount = 0;
+            for (char aChar : chars)
+            {
+                if (aChar == '-')
+                {
+                    cCount++;
+                }
+            }
+            pointsAmount = cCount;
+        }
+
     }
 
     @Override
