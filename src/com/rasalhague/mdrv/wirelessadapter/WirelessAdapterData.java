1c6a1f04a3145725af8e27c975a432d0571d54a4
RasAlhague <alexgr.dev@gmail.com>
2014-05-02 15:20:33 +0300

    Add realization

--- src/com/rasalhague/mdrv/wirelessadapter/WirelessAdapterData.java
+++ src/com/rasalhague/mdrv/wirelessadapter/WirelessAdapterData.java
@@ -2,5 +2,29 @@ package com.rasalhague.mdrv.wirelessadapter;
 
 public class WirelessAdapterData
 {
+    private byte channel;
+    private byte dB;
+    private int  IV;
 
+    public byte getChannel()
+    {
+        return channel;
+    }
+
+    public byte getdB()
+    {
+        return dB;
+    }
+
+    public int getIV()
+    {
+        return IV;
+    }
+
+    public WirelessAdapterData(byte channel, byte dB, int IV)
+    {
+        this.channel = channel;
+        this.dB = dB;
+        this.IV = IV;
+    }
 }
