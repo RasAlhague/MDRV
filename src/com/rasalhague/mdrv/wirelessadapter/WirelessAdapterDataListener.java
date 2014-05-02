e178d9dab8f5c3d5bb196b5ab6547de3ba0c38cc
RasAlhague <alexgr.dev@gmail.com>
2014-05-02 15:22:06 +0300

    Remove redundant parameter

--- src/com/rasalhague/mdrv/wirelessadapter/WirelessAdapterDataListener.java
+++ src/com/rasalhague/mdrv/wirelessadapter/WirelessAdapterDataListener.java
@@ -2,5 +2,5 @@ package com.rasalhague.mdrv.wirelessadapter;
 
 public interface WirelessAdapterDataListener
 {
-    public void wirelessAdapterDataEvent(int channel, WirelessAdapterData wirelessAdapterData);
+    public void wirelessAdapterDataEvent(WirelessAdapterData wirelessAdapterData);
 }
